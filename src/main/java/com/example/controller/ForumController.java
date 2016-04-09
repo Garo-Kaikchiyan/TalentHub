28312e1e6d9cf245ea105399e6d64b392dfb8aa2
Garo-Kaikchiyan <delta.lead@yahoo.com>
2016-04-09 21:27:15 +0300

    adding missed ForumController

--- src/main/java/com/example/controller/ForumController.java
+++ src/main/java/com/example/controller/ForumController.java
@@ -3,12 +3,15 @@ package com.example.controller;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import model.Question;
+import model.User;
 import model.dao.IQuestionDAO;
 import model.dao.IQuestionDAO.DataSource;
 
@@ -18,13 +21,14 @@ public class ForumController {
 	private ArrayList<Question> allPhpQuestions;
 	private ArrayList<Question> allAndroidQuestions;
 	private ArrayList<Question> allJSQuestions;
+	
 	@RequestMapping(value="/forum", method = RequestMethod.GET) 
 	public String goToForums() {
 		return "forum_types";
 	}
 	
 	@RequestMapping(value="/javaForum", method = RequestMethod.GET) 
-	public String goToJavaForums(Model model) {
+	public String goToJavaForums(HttpServletRequest req, Model model) {
 		if(allJavaQuestions == null)
 			try {
 				allJavaQuestions = IQuestionDAO.getDAO(DataSource.DB).getAllPosts("java");
@@ -32,19 +36,13 @@ public class ForumController {
 				System.out.println("Problem retrieving java forum questions");
 				e.printStackTrace();
 			}
-		model.addAttribute("questions", allJavaQuestions);
-		model.addAttribute("startIndex", 0);
-		if(allJavaQuestions != null){
-			if(allJavaQuestions.size()-1 >= 20)
-				model.addAttribute("endIndex", 20);
-			else
-				model.addAttribute("endIndex", allJavaQuestions.size() - 1);
-		}
+		req.getSession().setAttribute("questions", allJavaQuestions);
+		req.getSession().setAttribute("forum", "javaForum");
 		return "forum";
 	}
 	
 	@RequestMapping(value="/jsForum", method = RequestMethod.GET) 
-	public String goToJSForums(Model model) {
+	public String goToJSForums(HttpServletRequest req, Model model) {
 		if(allJavaQuestions == null)
 			try {
 				allJavaQuestions = IQuestionDAO.getDAO(DataSource.DB).getAllPosts("js");
@@ -52,19 +50,13 @@ public class ForumController {
 				System.out.println("Problem retrieving js forum questions");
 				e.printStackTrace();
 			}
-		if(allJavaQuestions != null){
-			model.addAttribute("questions", allJSQuestions);
-			model.addAttribute("startIndex", 0);
-			if(allJavaQuestions.size()-1 >= 20)
-				model.addAttribute("endIndex", 20);
-			else
-				model.addAttribute("endIndex", allJSQuestions.size() - 1);
-		}
+		req.getSession().setAttribute("questions", allJSQuestions);
+		req.getSession().setAttribute("forum", "jsForum");
 		return "forum";
 	}
 	
 	@RequestMapping(value="/androidForum", method = RequestMethod.GET) 
-	public String goToAndroidForums(Model model) {
+	public String goToAndroidForums(HttpServletRequest req, Model model) {
 		if(allJavaQuestions == null)
 			try {
 				allJavaQuestions = IQuestionDAO.getDAO(DataSource.DB).getAllPosts("android");
@@ -72,19 +64,14 @@ public class ForumController {
 				System.out.println("Problem retrieving android forum questions");
 				e.printStackTrace();
 			}
-		if(allJavaQuestions != null){
-			model.addAttribute("questions", allAndroidQuestions);
-			model.addAttribute("startIndex", 0);
-			if(allJavaQuestions.size()-1 >= 20)
-				model.addAttribute("endIndex", 20);
-			else
-				model.addAttribute("endIndex", allAndroidQuestions.size() - 1);
-		}
+		req.getSession().setAttribute("questions", allAndroidQuestions);
+		req.getSession().setAttribute("forum", "androidForum");
 		return "forum";
 	}
 	
 	@RequestMapping(value="/phpForum", method = RequestMethod.GET) 
-	public String goToPhpForums(Model model) {
+	public String goToPhpForums(HttpServletRequest req, Model model) {
+
 		if(allJavaQuestions == null)
 			try {
 				allJavaQuestions = IQuestionDAO.getDAO(DataSource.DB).getAllPosts("php");
@@ -92,17 +79,37 @@ public class ForumController {
 				System.out.println("Problem retrieving php forum questions");
 				e.printStackTrace();
 			}
-		if(allJavaQuestions != null){
-			model.addAttribute("questions", allPhpQuestions);
-			model.addAttribute("startIndex", 0);
-			if(allJavaQuestions.size()-1 >= 20)
-				model.addAttribute("endIndex", 20);
-			else
-				model.addAttribute("endIndex", allPhpQuestions.size() - 1);
-		}
+		req.getSession().setAttribute("questions", allPhpQuestions);
+		req.getSession().setAttribute("forum", "phpForum");
 		return "forum";
 	}
+
+	@RequestMapping(value="/createQuestion", method = RequestMethod.POST) 
+	public String createQuestion(HttpServletRequest req){
+		User u = (User) req.getSession().getAttribute("loggedUser");
+		Question q = new Question(req.getParameter("subject"), u.getEmail(), req.getParameter("text"));
+		IQuestionDAO.getDAO(DataSource.DB).addQuestion(u, q);
+		String forum = req.getSession().getAttribute("forum").toString();
+		addQuestionToList(forum, q);
+		return forum;
+	}
 	
-	
+	private void addQuestionToList(String type, Question q){
+		switch(type){
+		case "javaForum":
+			allJavaQuestions.add(q);
+			break;
+		case "androidForum":
+			allAndroidQuestions.add(q);
+			break;
+		case "phpForum":
+			allPhpQuestions.add(q);
+			break;
+		case "jsForum":
+			allJSQuestions.add(q);
+			break;
+			
+		}
+	}
 
 }
